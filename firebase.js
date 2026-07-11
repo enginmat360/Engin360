bb9f4db6ec7189f5abee6563c77a340586e7b120
enginmat360 <enginozcan82@gmail.com>
2026-07-11 22:39:39 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// ENGİN360 Firebase bağlantısı
+// Bu dosya tüm Firebase kullanan sayfalarda ortak kullanılacaktır.
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDyj5Pnv0tNeChf_t2XGdhiru5hjwKb6iIw",
+  authDomain: "engin360-56474.firebaseapp.com",
+  projectId: "engin360-56474",
+  storageBucket: "engin360-56474.firebasestorage.app",
+  messagingSenderId: "82163330076",
+  appId: "1:82163330076:web:f1c7b7b10e74ee8f4283d9",
+  measurementId: "G-1DVGR3MD40"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
